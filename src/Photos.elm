002658326105c2232c2e2048aee3a93eebdee70f
anovikov <a.novikov@me.com>
2019-10-13 22:27:51 +0200

    Adding comments

--- src/Photos.elm
+++ src/Photos.elm
@@ -2,19 +2,22 @@ module Photos exposing (view)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, src)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, disabled, placeholder, src, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 
 
 type Msg
-    = Like
-    | Unlike
+    = ToggleLike
+    | UpdateComment String
+    | SaveComment
 
 
 type alias Model =
     { url : String
     , caption : String
     , liked : Bool
+    , comments : List String
+    , newComment : String
     }
 
 
@@ -28,21 +31,55 @@ initialModel =
     { url = baseUrl ++ "1.jpg"
     , caption = "Surfing"
     , liked = False
+    , comments = [ "Cowabunga, dude!" ]
+    , newComment = ""
     }
+
+
+saveNewComment : Model -> Model
+saveNewComment model =
+    let
+        comment =
+            String.trim model.newComment
+    in
+    case comment of
+        "" ->
+            model
+
+        _ ->
+            { model
+                | comments = model.comments ++ [ comment ]
+                , newComment = ""
+            }
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Like ->
-            { model | liked = True }
+        ToggleLike ->
+            { model | liked = not model.liked }
 
-        Unlike ->
-            { model | liked = False }
+        UpdateComment comment ->
+            { model | newComment = comment }
+
+        SaveComment ->
+            saveNewComment model
 
 
 viewDetailedPhoto : Model -> Html Msg
-viewDetailedPhoto { url, caption, liked } =
+viewDetailedPhoto model =
+    div [ class "detailed-photo" ]
+        [ img [ src model.url ] []
+        , div [ class "photo-info" ]
+            [ viewLoveButton model.liked
+            , h2 [ class "caption" ] [ text model.caption ]
+            , viewComments model
+            ]
+        ]
+
+
+viewLoveButton : Bool -> Html Msg
+viewLoveButton liked =
     let
         buttonClass =
             if liked then
@@ -50,27 +87,52 @@ viewDetailedPhoto { url, caption, liked } =
 
             else
                 "far"
-
-        msg =
-            if liked then
-                Unlike
-
-            else
-                Like
     in
-    div [ class "detailed-photo" ]
-        [ img [ src url ] []
-        , div [ class "photo-info" ]
-            [ div [ class "like-button" ]
-                [ i
-                    [ class "fa-heart fa-2x"
-                    , class buttonClass
-                    , onClick msg
-                    ]
-                    []
-                ]
-            , h2 [ class "caption" ] [ text caption ]
+    div [ class "like-button" ]
+        [ i
+            [ class "fa-heart fa-2x"
+            , class buttonClass
+            , onClick ToggleLike
             ]
+            []
+        ]
+
+
+viewComments : Model -> Html Msg
+viewComments model =
+    div []
+        [ viewCommentList model.comments
+        , form [ class "new-comment", onSubmit SaveComment ]
+            [ input
+                [ type_ "text"
+                , placeholder "Add a comment"
+                , value model.newComment
+                , onInput UpdateComment
+                ]
+                []
+            , button [ disabled (String.isEmpty model.newComment) ] [ text "Save" ]
+            ]
+        ]
+
+
+viewCommentList : List String -> Html Msg
+viewCommentList comments =
+    case comments of
+        [] ->
+            text ""
+
+        _ ->
+            div [ class "comments" ]
+                [ ul []
+                    (List.map viewComment comments)
+                ]
+
+
+viewComment : String -> Html Msg
+viewComment comment =
+    li []
+        [ strong [] [ text "Comment:" ]
+        , text (" " ++ comment)
         ]
 
 
